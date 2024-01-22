dddd8db60d3510f4b2f949d92ff8acb44ac059f2
Silver-Eun <h22sang@naver.com>
2024-01-22 19:55:28 +0900

    ex5 edit

--- Baekjoon/step1/ex5.java
+++ Baekjoon/step1/ex5.java
@@ -6,8 +6,8 @@ public class ex5 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
 		
 		System.out.println(A/B);
 		
