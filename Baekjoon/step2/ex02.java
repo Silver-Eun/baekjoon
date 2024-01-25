5849fc2c12a938cdb614025cb76f0af417941d1f
Silver-Eun <h22sang@naver.com>
2024-01-25 20:31:38 +0900

    ex2
    grade

---
+++ Baekjoon/step2/ex02.java
@@ -0,0 +1,32 @@
+package step2;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		
+		if(0 <= A && A <= 100) {
+			if ( 90 <= A && A <= 100) {
+				System.out.println("A");
+			}
+			else if ( 80 <= A && A < 90) {
+				System.out.println("B");
+			}
+			else if ( 70 <= A && A < 80) {
+				System.out.println("C");
+			}
+			else if ( 60 <= A && A < 70) {
+				System.out.println("D");
+			}
+			else {
+				System.out.println("F");
+			}
+		}
+		
+		sc.close();
+	}
+
+}
