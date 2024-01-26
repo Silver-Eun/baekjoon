617cae26ea3c9ba85f70dad9f3c664c995155248
Silver-Eun <h22sang@naver.com>
2024-01-26 20:49:24 +0900

    ex3
    leap year

---
+++ Baekjoon/step2/ex03.java
@@ -0,0 +1,28 @@
+package step2;
+
+import java.util.Scanner;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		if(1 <= a && a <= 4000) {
+			if (a % 4 == 0) {
+				if( a % 100 != 0) {
+					System.out.println(1);
+				} else if (a % 400 == 0) {
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}
+			}else {
+				System.out.println(0);
+			}
+		}
+		
+		sc.close();
+	}
+
+}
