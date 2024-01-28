f5ab02a796dee53ce3c7ef3f2bea9bd7fe54383e
Silver-Eun <h22sang@naver.com>
2024-01-28 18:53:27 +0900

    ex2
    Enter two numbers and output the sum

---
+++ Baekjoon/step3/ex02.java
@@ -0,0 +1,21 @@
+package step3;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		
+		for(int i = a; i > 0; i--) {
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			System.out.println(b+c);
+		}
+		
+		sc.close();
+	}
+
+}
