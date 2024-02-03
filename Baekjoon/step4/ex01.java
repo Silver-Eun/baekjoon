65ff37261f23f6ebe2c51bb0822ba2f95e5098e9
Silver-Eun <h22sang@naver.com>
2024-02-03 15:29:47 +0900

    ex2
    Enter array, x  and ouput less then x

--- Baekjoon/step4/ex01.java
+++ Baekjoon/step4/ex01.java
@@ -6,27 +6,26 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int count = 0;
 		int a = sc.nextInt();
-		
+
 		int[] arr = new int[a];
-		
-		
-		for(int i = 0; i < a; i++) {
+
+		for (int i = 0; i < a; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
 		int b = sc.nextInt();
-		
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] == b) {
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == b) {
 				count++;
 			}
 		}
-		
+
 		System.out.println(count);
-		
+
 		sc.close();
 	}
 
