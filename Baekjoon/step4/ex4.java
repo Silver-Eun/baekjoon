5be501e6c17e0456bd207d2fca36f67718590e48
Silver-Eun <h22sang@naver.com>
2024-02-03 20:17:21 +0900

    ex4
    Enter array and output max

---
+++ Baekjoon/step4/ex4.java
@@ -0,0 +1,28 @@
+package step4;
+
+import java.util.Scanner;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[8];
+
+		int max = arr[0];
+		int count = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+
+			if (max < arr[i]) {
+				max = arr[i];
+				count = i + 1;
+			}
+		}
+		sc.close();
+		
+		System.out.println(max);
+		System.out.println(count);
+	}
+
+}
