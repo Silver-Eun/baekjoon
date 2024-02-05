85879d76a6b607dae3ce9cfeeea3e8cc86dadeb1
Silver-Eun <h22sang@naver.com>
2024-02-06 00:14:35 +0900

    ex1
    String

--- Baekjoon/step5/ex01.java
+++ Baekjoon/step5/ex01.java
@@ -1,6 +1,18 @@
 package step5;
 
+import java.util.Scanner;
+
 public class ex01 {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.next();
+		int a = sc.nextInt();
+		
+		System.out.println(S.charAt(a-1));
+		
+		sc.close();
+	}
 
 }
