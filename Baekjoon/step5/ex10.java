90beafe6d3f5b827ae4e23768ec14e03093d562a
Silver-Eun <h22sang@naver.com>
2024-02-09 16:47:46 +0900

    ex2
    Chess

--- Baekjoon/step5/ex10.java
+++ Baekjoon/step5/ex10.java
@@ -3,6 +3,7 @@ package step5;
 import java.util.Scanner;
 
 public class ex10 {
+	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
---
+++ Baekjoon/step6/ex02.java
@@ -0,0 +1,23 @@
+package step6;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int k = sc.nextInt();
+		int q = sc.nextInt();
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+
+		System.out.println((1 - k) + " " + (1 - q) + " " + (2 - l)
+				+ " " + (2 - b) + " " + (2 - n) + " " + (8 - p));
+	
+		sc.close();
+	}
+
+}
