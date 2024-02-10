ea8dffff9aea3738b2600cbfc821ebbe9a27f064
Silver-Eun <h22sang@naver.com>
2024-02-10 23:50:22 +0900

    ex4
    Palindrome

---
+++ Baekjoon/step6/ex04.java
@@ -0,0 +1,25 @@
+package step6;
+
+import java.util.Scanner;
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String S = sc.next();
+		int len = S.length();
+		int ans = 1;
+
+		for (int i = 0; i < len / 2; i++) {
+			if (S.charAt(i) != S.charAt(len - 1 - i)) {
+				ans = 0;
+			}
+		}
+
+		System.out.println(ans);
+
+		sc.close();
+	}
+
+}
