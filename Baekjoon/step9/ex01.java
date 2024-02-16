fbee12b937acd61b363f133066ebd107d3ff2472
Silver-Eun <h22sang@naver.com>
2024-02-16 23:15:11 +0900

    ex1
    Multiple and submultiple

--- Baekjoon/step9/ex01.java
+++ Baekjoon/step9/ex01.java
@@ -1,9 +1,30 @@
 package step9;
 
+import java.util.Scanner;
+
 public class ex01 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			if (a == 0 && b == 0)
+				break;
+
+			if (b % a == 0) {
+				System.out.println("factor");
+			} else if (a % b == 0) {
+				System.out.println("multiple");
+			} else {
+				System.out.println("neither");
+			}
+		}
 		
+		sc.close();
 	}
 
 }
