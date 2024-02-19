40badb8e744decbcf0168a2a741c5e4cee2e92ff
Silver-Eun <h22sang@naver.com>
2024-02-19 22:47:05 +0900

    ex4
    Perimeter of pyramid

---
+++ Baekjoon/step10/ex04.java
@@ -0,0 +1,18 @@
+package step10;
+
+import java.util.Scanner;
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 정사각형 개수
+		long N = sc.nextLong();
+		
+		System.out.println(4*N);
+		
+		sc.close();
+	}
+
+}
