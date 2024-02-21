71b7b6ce22044006f272fa52c6b44b5018c2f24f
Silver-Eun <h22sang@naver.com>
2024-02-21 22:22:12 +0900

    Step - 11 ex1
    Algorithm
    
    Time to run the algorithm1

---
+++ Baekjoon/step11/ex01.java
@@ -0,0 +1,12 @@
+package step11;
+
+import java.io.IOException;
+
+public class ex01 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(1);
+		System.out.println(0);
+	}
+
+}
