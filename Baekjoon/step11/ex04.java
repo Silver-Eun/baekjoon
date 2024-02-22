1c18b526731e7158f4128d00fafc08fff9d91e4c
Silver-Eun <h22sang@naver.com>
2024-02-22 17:44:21 +0900

    ex4
    Time to run the algorithm4

---
+++ Baekjoon/step11/ex04.java
@@ -0,0 +1,18 @@
+package step11;
+
+import java.util.Scanner;
+
+public class ex04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		Long n = sc.nextLong();
+
+		System.out.println((n*(n-1))/2);
+		System.out.println(2);
+		
+		sc.close();
+	}
+
+}
