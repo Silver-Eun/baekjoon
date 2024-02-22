cec5c30db4c98400bc753caf5a6d7f4f8de2b62a
Silver-Eun <h22sang@naver.com>
2024-02-22 17:36:53 +0900

    ex3
    Time to run the algorithm3

---
+++ Baekjoon/step11/ex03.java
@@ -0,0 +1,18 @@
+package step11;
+
+import java.util.Scanner;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Long n = sc.nextLong();
+		
+		System.out.println(n*n);
+		System.out.println(2);
+		
+		sc.close();
+	}
+
+}
