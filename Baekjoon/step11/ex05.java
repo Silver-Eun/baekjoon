c51f17e29b05adb242ad5a71044e4b7b62868497
Silver-Eun <h22sang@naver.com>
2024-02-22 17:53:33 +0900

    ex5
    Time to run the algorithm5

---
+++ Baekjoon/step11/ex05.java
@@ -0,0 +1,18 @@
+package step11;
+
+import java.util.Scanner;
+
+public class ex05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Long n = sc.nextLong();
+		
+		System.out.println(n*n*n);
+		System.out.println(3);
+		
+		sc.close();
+	}
+
+}
