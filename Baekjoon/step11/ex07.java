b24b72d33fcd30e040b08b504cd8c88a6845773e
Silver-Eun <h22sang@naver.com>
2024-02-22 20:26:17 +0900

    ex7
    Asymptotic notation1

---
+++ Baekjoon/step11/ex07.java
@@ -0,0 +1,24 @@
+package step11;
+
+import java.util.Scanner;
+
+public class ex07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a1 = sc.nextInt();
+		int a0 = sc.nextInt();
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+
+		if (a1 * n + a0 <= c * n && c >= a1) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+
+		sc.close();
+	}
+
+}
