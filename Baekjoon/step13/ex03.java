1fc2b9ffcb74eb60e8ed9ed97cb7a363bede344f
Silver-Eun <h22sang@naver.com>
2024-02-27 15:05:23 +0900

    ex3
    Cutline

---
+++ Baekjoon/step13/ex03.java
@@ -0,0 +1,27 @@
+package step13;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+
+		System.out.println(arr[N - k]);
+		
+		sc.close();
+	}
+
+}
