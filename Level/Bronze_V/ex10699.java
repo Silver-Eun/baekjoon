ea5d57be847614ee50f676a469f962760b6ca362
Silver-Eun <h22sang@naver.com>
2024-06-11 22:30:35 +0900

    Bronze V
    LocalDate

---
+++ Level/Bronze_V/ex10699.java
@@ -0,0 +1,12 @@
+package Bronze_V;
+
+import java.time.LocalDate;
+
+public class ex10699 {
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		System.out.println(now);
+	}
+
+}
