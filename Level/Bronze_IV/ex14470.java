b628571e6957cc53a26db4c4a2feb9c226994a4a
Silver-Eun <h22sang@naver.com>
2024-07-01 19:16:17 +0900

    Bronze IV
    cooking time

---
+++ Level/Bronze_IV/ex14470.java
@@ -0,0 +1,34 @@
+package Bronze_IV;
+
+import java.util.Scanner;
+
+public class ex14470 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		int E = sc.nextInt();
+
+		sc.close();
+
+		int time = 0;
+
+		if (A < 0) {
+			time += Math.abs(0 - A) * 5;
+			time += D;
+			time += B * E;
+		} else if (A == 0) {
+			time += D;
+			time += B * E;
+		} else if (0 < A) {
+			time += (B - A) * E;
+		}
+
+		System.out.println(time);
+	}
+
+}
