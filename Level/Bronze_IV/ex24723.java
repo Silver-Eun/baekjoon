6437e8520c5a8e74f31d4f9ff09a2efcef40b6b6
Silver-Eun <h22sang@naver.com>
2024-07-08 20:57:02 +0900

    Bronze IV
    green tower

---
+++ Level/Bronze_IV/ex24723.java
@@ -0,0 +1,17 @@
+package Bronze_IV;
+
+import java.util.Scanner;
+
+public class ex24723 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		sc.close();
+
+		System.out.println(Math.pow(2, N));
+	}
+
+}
