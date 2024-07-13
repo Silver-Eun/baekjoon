73de768cf15283fbf2b697c99987e6e9a6b64f20
Silver-Eun <h22sang@naver.com>
2024-07-13 14:41:55 +0900

    Bronze IV
    name

---
+++ Level/Bronze_IV/ex30501.java
@@ -0,0 +1,21 @@
+package Bronze_IV;
+
+import java.util.Scanner;
+
+public class ex30501 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = Integer.parseInt(sc.nextLine());
+		
+		for(int i = 0; i < N; i++) {
+			String a = sc.nextLine();
+			for(int j = 0; j < a.length(); j++) {
+				if(a.charAt(j) == 'S')
+					System.out.println(a);
+			}
+		}
+	}
+
+}
