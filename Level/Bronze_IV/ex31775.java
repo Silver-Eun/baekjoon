39bc7dd94613dff09e83085724f7a014b055db92
Silver-Eun <h22sang@naver.com>
2024-07-20 16:57:21 +0900

    Bronze IV
    global phoenix

---
+++ Level/Bronze_IV/ex31775.java
@@ -0,0 +1,35 @@
+package Bronze_IV;
+
+import java.util.Scanner;
+
+public class ex31775 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int l = 0;
+		int k = 0;
+		int p = 0;
+
+		for (int i = 0; i < 3; i++) {
+			String s = sc.next();
+			if (s.charAt(0) == 'l') {
+				l = 1;
+			} else if (s.charAt(0) == 'k') {
+				k = 1;
+			} else if (s.charAt(0) == 'p') {
+				p = 1;
+			}
+		}
+
+		if (l == 1 && k == 1 && p == 1) {
+			System.out.println("GLOBAL");
+		} else {
+			System.out.println("PONIX");
+		}
+
+		sc.close();
+
+	}
+
+}
