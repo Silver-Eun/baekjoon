a2c5a00b1930f9746b35e969c9c8a476d1bc28e7
Silver-Eun <h22sang@naver.com>
2024-07-29 18:11:17 +0900

    Bronze III
    cellphone price

---
+++ Level/Bronze_III/ex01267.java
@@ -0,0 +1,32 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex01267 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = Integer.parseInt(sc.nextLine());
+		String A = sc.nextLine();
+		String[] arr = A.split(" ");
+		int Y = 0;
+		int M = 0;
+
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(arr[i]);
+			// 영식요금제
+			Y += (a / 30 + 1) * 10;
+			// 민식요금제
+			M += (a / 60 + 1) * 15;
+		}
+		if (Y < M) {
+			System.out.println("Y " + Y);
+		} else if (M < Y) {
+			System.out.println("M " + M);
+		} else {
+			System.out.println("Y M " + Y);
+		}
+	}
+
+}
