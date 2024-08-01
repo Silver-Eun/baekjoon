e8837669652ae287a80672d0daf761f5fcaa2692
Silver-Eun <h22sang@naver.com>
2024-08-01 18:32:47 +0900

    Bronze III
    make star - 5

--- Level/Bronze_III/ex02442.java
+++ Level/Bronze_III/ex02442.java
@@ -2,20 +2,20 @@ package Bronze_III;
 
 import java.util.Scanner;
 
-public class ex02441 {
+public class ex02442 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
-		
+
 		sc.close();
 
 		for (int i = 0; i < N; i++) {
-			for (int j = i; j > 0; j--) {
+			for (int j = (N - i); j > 1; j--) {
 				System.out.print(" ");
 			}
-			for (int j = 0; j < N - i; j++) {
+			for (int j = 2 * (N - i - 1); j <= 2 * (N - 1); j++) {
 				System.out.print("*");
 			}
 			System.out.println();
