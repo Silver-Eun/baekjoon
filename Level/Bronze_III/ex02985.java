d1647b7212bf4f5677bd4f57c468733d95b26d96
Silver-Eun <h22sang@naver.com>
2024-08-12 18:23:02 +0900

    Bronze III
    four basic operations

---
+++ Level/Bronze_III/ex02985.java
@@ -0,0 +1,37 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex02985 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] a = sc.nextLine().split(" ");
+
+		int A = Integer.parseInt(a[0]);
+		int B = Integer.parseInt(a[1]);
+		int C = Integer.parseInt(a[2]);
+		
+		sc.close();
+
+		if (A + B == C) {
+			System.out.println(A + "+" + B + "=" + C);
+		} else if (A - B == C) {
+			System.out.println(A + "-" + B + "=" + C);
+		} else if (A * B == C) {
+			System.out.println(A + "*" + B + "=" + C);
+		} else if (A / B == C) {
+			System.out.println(A + "/" + B + "=" + C);
+		} else if (A == B + C) {
+			System.out.println(A + "=" + B + "+" + C);
+		} else if (A == B - C) {
+			System.out.println(A + "=" + B + "-" + C);
+		} else if (A == B * C) {
+			System.out.println(A + "=" + B + "*" + C);
+		} else if (A == B / C) {
+			System.out.println(A + "=" + B + "/" + C);
+		}
+	}
+
+}
