6d0ec5de3fa5f9c39a55664c3dcb4eeb7c1feb69
Silver-Eun <h22sang@naver.com>
2024-08-21 17:29:46 +0900

    Bronze III
    find cubed number

---
+++ Level/Bronze_III/ex04690.java
@@ -0,0 +1,18 @@
+package Bronze_III;
+
+public class ex04690 {
+
+	public static void main(String[] args) {
+		for (int a = 2; a <= 100; a++) {
+			for (int b = 2; b <= a; b++) {
+				for (int c = b; c <= a; c++) {
+					for (int d = c; d <= a; d++) {
+						if (a * a * a == b * b * b + c * c * c + d * d * d) {
+							System.out.println("Cube = " + a + ", Triple = (" + b + "," + c + "," + d + ")");
+						}
+					}
+				}
+			}
+		}
+	}
+}
