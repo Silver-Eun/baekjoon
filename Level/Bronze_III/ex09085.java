8633d665c15190c6523e4b179d7dbbbde34ec9d2
Silver-Eun <h22sang@naver.com>
2024-09-04 00:16:52 +0900

    Bronze III
    addition

---
+++ Level/Bronze_III/ex09085.java
@@ -0,0 +1,31 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex09085 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		
+		for(int i = 0; i < T; i++) {
+			int sum = 0;
+			int N = sc.nextInt();
+			
+			sc.nextLine();
+			
+			String[] a = sc.nextLine().split(" ");
+			
+			for(int j = 0; j < N; j++) {
+				sum += Integer.parseInt(a[j]);
+			}
+			
+			System.out.println(sum);
+		}
+		
+		sc.close();
+	}
+
+}
