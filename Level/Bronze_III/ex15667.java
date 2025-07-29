ec685e3aa64e7c0489bc464300df0c6656fdcf18
Silver-Eun <h22sang@naver.com>
2025-07-29 23:57:16 +0900

    Bronze III
    fireworks

---
+++ Level/Bronze_III/ex15667.java
@@ -0,0 +1,22 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex15667 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int K = 1;
+
+		while (K * K + K <= N - 1) {
+			K++;
+		}
+
+		sc.close();
+
+		System.out.println(K - 1);
+	}
+
+}
