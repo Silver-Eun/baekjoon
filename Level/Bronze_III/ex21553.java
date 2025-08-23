d9e3598774f05cca4ad1cb0ab29bc3e5d184863b
Silver-Eun <h22sang@naver.com>
2025-08-23 18:43:27 +0900

    Bronze III
    make password

---
+++ Level/Bronze_III/ex21553.java
@@ -0,0 +1,16 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex21553 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String A = sc.nextLine();
+		String P = sc.nextLine();
+		
+		System.out.println(P);
+	}
+
+}
