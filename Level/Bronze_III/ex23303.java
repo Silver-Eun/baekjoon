d72bdc2429f7d5b26cf6ca9417d7524e1c6cf078
Silver-Eun <h22sang@naver.com>
2025-08-26 22:38:47 +0900

    Bronze III
    D2

---
+++ Level/Bronze_III/ex23303.java
@@ -0,0 +1,20 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex23303 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String s = sc.nextLine();
+
+		if (s.contains("D2") || s.contains("d2")) {
+			System.out.println("D2");
+		} else {
+			System.out.println("unrated");
+		}
+
+		sc.close();
+	}
+}
