bec45353b27252709f074e1af029a44efbafa89e
Silver-Eun <h22sang@naver.com>
2025-08-30 22:26:48 +0900

    Bronze III
    take a picture of a @ - ㅂ

---
+++ Level/Bronze_III/ex23808.java
@@ -0,0 +1,35 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex23808 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			System.out.println("@".repeat(N) + "   ".repeat(N) + "@".repeat(N));
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println("@".repeat(N) + "   ".repeat(N) + "@".repeat(N));
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println("@".repeat(5*N));
+		}
+		
+		for (int i = 0; i < N; i++) {
+			System.out.println("@".repeat(N) + "   ".repeat(N) + "@".repeat(N));
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println("@".repeat(5*N));
+		}
+
+		sc.close();
+	}
+	
+}
