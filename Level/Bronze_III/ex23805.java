b39c6753b7974206ecac14bf31241d3b8eaa4187
Silver-Eun <h22sang@naver.com>
2025-08-30 22:02:24 +0900

    Bronze III
    take a picture of a @ - ㄹ

---
+++ Level/Bronze_III/ex23805.java
@@ -0,0 +1,60 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex23805 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@@@");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < 3 * N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < N; j++) {
+				System.out.print("@@@");
+			}
+			System.out.println();
+		}
+
+		sc.close();
+	}
+
+}
