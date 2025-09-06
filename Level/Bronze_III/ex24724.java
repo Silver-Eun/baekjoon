71a1be1c1bad3b70ef0072e756c0716c31832591
Silver-Eun <h22sang@naver.com>
2025-09-06 21:50:56 +0900

    Bronze III
    Hyundai Mobis

---
+++ Level/Bronze_III/ex24724.java
@@ -0,0 +1,27 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex24724 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			for(int j = 0; j < N; j++) {
+				int u = sc.nextInt();
+				int v = sc.nextInt();
+			}
+			System.out.println("Material Management " + (i + 1));
+			System.out.println("Classification ---- End!");
+		}
+		
+		sc.close();
+	}
+
+}
