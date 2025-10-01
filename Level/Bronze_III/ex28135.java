41623a5c3fd4cb318afb0319b5e4ce8957d7b6b5
Silver-Eun <h22sang@naver.com>
2025-10-02 00:20:21 +0900

    Bronze III
    Since 1973

---
+++ Level/Bronze_III/ex28135.java
@@ -0,0 +1,25 @@
+package Bronze_III;
+
+import java.io.*;
+
+public class ex28135 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int answer = 0;
+
+		for (int i = 1; i <= n; i++) {
+			if (String.valueOf(i).contains("50")) {
+				answer += 2;
+			} else {
+				answer += 1;
+			}
+		}
+
+		answer = String.valueOf(n).contains("50") ? answer -= 1 : answer;
+		System.out.println(answer);
+	}
+
+}
