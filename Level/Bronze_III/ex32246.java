aa425161d58e21997802b25ee3d2af4259e51f94
Silver-Eun <h22sang@naver.com>
2025-10-25 00:00:05 +0900

    Bronze III
    stop bingo

---
+++ Level/Bronze_III/ex32246.java
@@ -0,0 +1,20 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex32246 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		if(n == 2)
+			System.out.println(3);
+		else
+			System.out.println(n);
+		
+		sc.close();
+	}
+
+}
