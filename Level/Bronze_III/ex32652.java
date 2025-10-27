a34c6fce48d4f8a5f2c28566f98697654cfccfd2
Silver-Eun <h22sang@naver.com>
2025-10-27 23:56:13 +0900

    Bronze III
    AKARAKA II

---
+++ Level/Bronze_III/ex32652.java
@@ -0,0 +1,17 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex32652 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int K = sc.nextInt();
+
+		System.out.println("AKARAKA" + "RAKA".repeat(K - 1));
+
+		sc.close();
+	}
+
+}
