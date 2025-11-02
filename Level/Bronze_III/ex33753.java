23834b2f6e78b94384fb407019751b3897bd3d1c
Silver-Eun <h22sang@naver.com>
2025-11-02 23:58:39 +0900

    Bronze III
    parking fee

---
+++ Level/Bronze_III/ex33753.java
@@ -0,0 +1,29 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex33753 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+
+		int T = sc.nextInt();
+
+		int m = 0;
+
+		if (T <= 30) {
+			m = A;
+		} else {
+			m = A + ((T - 30) / B * C);
+		}
+
+		System.out.println(m);
+
+		sc.close();
+	}
+
+}
