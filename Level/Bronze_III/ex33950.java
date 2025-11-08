cf7bc1b253f2c736f1ac862c735398c6c40a98f6
Silver-Eun <h22sang@naver.com>
2025-11-08 23:57:11 +0900

    Bronze III
    again ponix

---
+++ Level/Bronze_III/ex33950.java
@@ -0,0 +1,15 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex33950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		
+		sc.close();
+	}
+
+}
