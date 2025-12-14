e33382df2d63eaa005ab791daa8583ca3f473e1e
Silver-Eun <h22sang@naver.com>
2025-12-15 03:06:18 +0900

    Bronze III
    zebra & lion

---
+++ Level/Bronze_III/ex34759.java
@@ -0,0 +1,19 @@
+package Bronze_III;
+
+import java.util.Scanner;
+
+public class ex34759 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long N = sc.nextLong();
+		
+		long Z = 2 * N;
+
+		System.out.println(Z);
+
+		sc.close();
+	}
+
+}
