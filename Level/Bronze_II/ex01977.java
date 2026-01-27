42cf95c5937198be4075d18c3935b0d6fd3bb411
Silver-Eun <h22sang@naver.com>
2026-01-27 23:59:48 +0900

    Bronze II
    perfect square number

---
+++ Level/Bronze_II/ex01977.java
@@ -0,0 +1,23 @@
+package Bronze_II;
+
+import java.util.Scanner;
+
+public class ex01977 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+		
+		int sum = 0;
+		int min = -1;
+		
+		for(int i = 1; i <= 100; i++) {
+			
+		}
+		
+		sc.close();
+	}
+
+}
