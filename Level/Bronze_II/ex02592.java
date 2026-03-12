a3e857248163d474a6621b17b7568d6a9b8dcd84
Silver-Eun <h22sang@naver.com>
2026-03-12 23:59:14 +0900

    Bronze II
    representative value

---
+++ Level/Bronze_II/ex02592.java
@@ -0,0 +1,35 @@
+package Bronze_II;
+
+import java.util.Scanner;
+
+public class ex02592 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] cnt = new int[100];
+		
+		int sum = 0;
+
+		for(int i = 0; i < 10; i++) {
+			int N = sc.nextInt();
+			
+			cnt[N/10]++;
+			
+			sum += N;
+		}
+		
+		int max = 0;
+		int mode = 0;
+
+		for (int i = 0; i < 100; i++) {
+		    if (cnt[i] > max) {
+		        max = cnt[i];
+		        mode = i * 10;
+		    }
+		}
+		
+		sc.close();
+	}
+
+}
