47798ae483c545602efc2cbbc794b75a2968cb22
Silver-Eun <h22sang@naver.com>
2026-03-28 21:48:25 +0900

    Bronze II
    floor of dormitory

---
+++ Level/Bronze_II/ex2858.java
@@ -0,0 +1,32 @@
+package Bronze_II;
+
+import java.util.Scanner;
+
+public class ex2858 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int R = sc.nextInt();
+		int B = sc.nextInt();
+
+		int m = R + B;
+
+		for (int L = 1; L <= m; L++) {
+			if (m % L == 0) {
+				int W = m / L;
+				if ((L - 2) * (W - 2) == B) {
+					if (L >= W)
+						System.out.println(L + " " + W);
+					else
+						System.out.println(W + " " + L);
+					break;
+				}
+			}
+
+		}
+
+		sc.close();
+	}
+
+}
