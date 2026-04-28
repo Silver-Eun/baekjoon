85bf82bc3547225477f159d984a158f849d7461f
Silver_Eun <h22sang@naver.com>
2026-04-28 19:53:57 +0900

    solve(Lv0): 문자 출력

---
+++ Programmers/Lv0/ex340207.java
@@ -0,0 +1,12 @@
+package Lv0;
+
+public class ex340207 {
+
+    public static void main(String[] args) {
+        String message = " Let 's go! ";
+
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+    }
+
+}
