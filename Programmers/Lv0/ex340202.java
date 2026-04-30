a49191dd50b99acc7faae76fb75e826157f6c050
Silver_Eun <h22sang@naver.com>
2026-04-30 23:57:16 +0900

    solve(Lv0): 물 부족

---
+++ Programmers/Lv0/ex340202.java
@@ -0,0 +1,16 @@
+package Lv0;
+
+public class ex340202 {
+
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for (int i = 0; i < change.length; i++) {
+            usage = total_usage * change[i] / 100;
+            total_usage += usage;
+            if (total_usage > storage) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
