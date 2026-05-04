2a5dc11b9e23dbfe4fb898f0cd764df8a748a37c
Silver_Eun <h22sang@naver.com>
2026-05-04 20:06:36 +0900

    solve(Lv0): 문자열 반복해서 출력하기

---
+++ Programmers/Lv0/ex181950.java
@@ -0,0 +1,17 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181950 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(str);
+        }
+    }
+
+}
