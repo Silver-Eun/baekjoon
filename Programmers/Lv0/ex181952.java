95371f1b046be399408d9d09581ef53aaa8a0f6e
Silver_Eun <h22sang@naver.com>
2026-05-04 20:03:19 +0900

    solve(Lv0): 문자열 출력하기

---
+++ Programmers/Lv0/ex181952.java
@@ -0,0 +1,12 @@
+package Lv0;
+
+import java.util.Scanner;
+
+public class ex181952 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+    }
+
+}
